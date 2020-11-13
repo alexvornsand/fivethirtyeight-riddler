e3d8429d09d4589715cf50a57a091cebfb8e301c
ajvornsand <57457024+ajvornsand@users.noreply.github.com>
2020-11-13 10:04:27 -0800

    Create riddler.py

---
+++ 2020-11-13/Riddler Classic/riddler.py
@@ -0,0 +1,8 @@
+# https://fivethirtyeight.com/features/can-you-snatch-defeat-from-the-jaws-of-victory/
+
+from math import comb
+
+for N in reversed(range(102)): # N is the number of remaining flips
+    for n in range(N): # n is the number of flips won so far
+        if 51 - n <= N
+        for m in range(N-n) # m is the number flips left to win to exceed 51
