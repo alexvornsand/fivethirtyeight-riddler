1f8ac58139c6c7b834c35e42d025473ccf2a5f8c
alexvornsand <ajvornsand@ucdavis.edu>
2020-11-24 11:04:07 -0800

    Create riddler.jl

---
+++ 2020-11-20/Riddler Classic/riddler.jl
@@ -0,0 +1,26 @@
+# https://fivethirtyeight.com/features/can-you-pass-the-cranberry-sauce/
+
+using Pkg
+Pkg.add("StatsBase")
+using StatsBase
+
+function findLastToSauce(k)
+    seats = [0:(k-1);]
+    wantsSauce = [1:(k-1);]
+    hasSauce = 0
+    while length(wantsSauce) > 1
+        passTo = sample([-1,1])
+        hasSauce = mod(hasSauce + passTo, k)
+        if hasSauce in wantsSauce
+            deleteat!(wantsSauce, wantsSauce .== hasSauce)
+        end
+    end
+    return wantsSauce
+end
+
+function findLastToSauceRepeatedly(n, k)
+    x = [findLastToSauce(k) for i in 1:n]
+    return proportionmap(x)
+end
+
+findLastToSauceRepeatedly(100000, 20)
