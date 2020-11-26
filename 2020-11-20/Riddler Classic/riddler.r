9a1025e40eeb5af06ef6b05833cf28c4ff07ddb8
alexvornsand <ajvornsand@ucdavis.edu>
2020-11-26 09:33:39 -0800

    add riddler.gif

--- 2020-11-20/Riddler Classic/riddler.r
+++ 2020-11-20/Riddler Classic/riddler.r
@@ -1,5 +1,8 @@
 # https://fivethirtyeight.com/features/can-you-pass-the-cranberry-sauce/
 
+library(ggplot2)
+library(gganimate)
+
 findLastToSauce <- function(k){
   seats <- 0:(k-1)
   wantsSauce <- 1:(k-1)
@@ -20,3 +23,93 @@ findLastToSauceRepeatedly <- function(n, k){
 }
 
 findLastToSauceRepeatedly(100000, 20)
+
+buildGifData <- function(k){
+  k <- 20
+  cranData <- data.frame(
+    turn = rep(0, k),
+    seat = 0:(k-1),
+    cranSauce = c(2, rep(0, (k-1)))
+  )
+  seats <- 0:(k-1)
+  wantsSauce <- 1:(k-1)
+  hasSauce <- 0
+  period <- 0
+  while(length(wantsSauce) > 1){
+    passTo <- sample(c(-1,1), 1)
+    hasSauce <- (hasSauce + passTo) %% k
+    if(hasSauce %in% wantsSauce){
+      wantsSauce <- wantsSauce[wantsSauce != hasSauce]
+    }
+    period <- period + 1
+    newState <- data.frame(
+      turn = rep(period, k),
+      seat = 0:(k-1),
+      cranSauce = rep(1, k)
+    )
+    newState$cranSauce[which(newState$seat == hasSauce)] <- 2
+    newState$cranSauce[which(newState$seat %in% wantsSauce)] <- 0
+    cranData <- rbind(cranData, newState)
+  }
+  finalState <- data.frame(
+    turn = rep(period + 1, k),
+    seat = 0:(k-1),
+    cranSauce = rep(1, k)
+  )
+  finalState$cranSauce[which(finalState$seat == wantsSauce)] <- 2
+  cranData <- rbind(cranData, finalState)
+  cranData$x <- cos(cranData$seat * 2 * pi / k)
+  cranData$y <- sin(cranData$seat * 2 * pi / k)
+  cranData$cranSauce <- as.factor(cranData$cranSauce)
+  return(cranData)
+}
+
+gifData <- buildGifData(20)
+
+g <- ggplot(
+  gifData,
+  aes(
+    x,
+    y
+  )
+) +
+  geom_point(
+    aes(
+      fill = cranSauce,
+      group = seq_along(turn)
+    ),
+    pch = 21,
+    size = 10,
+    color = "#1a1a1a"
+  ) +
+  scale_fill_manual(values = c('0' = '#ffffff', '1' = '#686868', '2' = '#007acc')) +
+  theme_bw() +
+  theme(
+    panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_blank(),
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    legend.position = 'none'
+  ) +
+  transition_manual(turn)
+
+animate(
+  plot = g,
+  nframes = length(unique(gifData$turn)),
+  fps = 4,
+  start_pause = 4,
+  end_pause = 4
+)
+
+anim_save(
+  'riddler.gif',
+  width = 4,
+  units = 'in',
+  dpi = 'retina'
+)
